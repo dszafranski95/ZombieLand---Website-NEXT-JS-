9fc64d0cfd2744b2d77ba0eaf117e08a1ca66a25
Dorian Szafranski <130157158+travilabs@users.noreply.github.com>
2023-11-25 21:51:26 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  // Ustawienie, aby Next.js wygenerował statyczną stronę
+  output: 'standalone',
+  // Możesz dodać dodatkowe konfiguracje tutaj
+};
